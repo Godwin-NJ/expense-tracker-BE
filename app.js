0fdd7ca11835364d21abf25669a7545e4a701318
godwin-nj <amadigodwin7@gmail.com>
2022-01-26 15:45:58 +0100

    adding security features

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
 require('dotenv').config()
 const express = require('express')
+
+
+
+//  extra security features 
+const helmet = require('helmet')
+const xss = require('xss-clean')
+const cors = require('cors')
+const rateLimit = require('express-rate-limit')
+
+// creating express server 
 const app = express()
-
-
+// feature middleware 
+app.set('trust proxy', 1);
+app.use(express.json())
+app.use(helmet())
+app.use(xss())
+app.use(cors())
+app.use(rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100  // limit each IP to 100 requests per windowMs
+  }))
 
 // connect to db 
 const connectDB = require('./db/connect')
@@ -18,13 +36,13 @@ const userEntryRouter = require('./routes/userExpense')
 const userLogRouter = require('./routes/userLog')
 
 
-
-app.use(express.json())
-
+// routes middleware
 app.use('/api/v1/expense',userEntryRouter)
 app.use('/api/v1/user',userLogRouter)
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 5000
+
+
 
 const start = async() => {
     try {
