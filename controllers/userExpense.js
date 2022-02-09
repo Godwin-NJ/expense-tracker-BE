e876dd0a75bea3aa2eec36b85d946ecbfb06e978
godwin-nj <amadigodwin7@gmail.com>
2022-02-09 20:02:14 +0100

    adjust expenseentry model

--- controllers/userExpense.js
+++ controllers/userExpense.js
@@ -8,17 +8,20 @@ const getAllEntry = async(req,res) => {
 }
 
 const createEntry = async(req,res) => {
-   
+    // console.log('i got here')
+//    console.log(req.body)
   const entry = await expenseUser.create({...req.body})
   
 //   when expense already exist throw and error 
 //   if(req.body.title && req.body.date){
 //       throw new Error('expense already exist')
 //   }
+
   res.status(StatusCodes.CREATED).json({entry})
 }
 
 const updateEntry = async(req,res) => {
+    
     const{
         body : {title,expenseType,amount},
         params : {id:expenseID}
